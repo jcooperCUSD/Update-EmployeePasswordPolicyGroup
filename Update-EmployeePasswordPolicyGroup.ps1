64877fea1c635ae4893e69f0b6073d37c91b7e36
Justin Cooper <jcooper@chicousd.org>
2019-05-09 15:57:06 -0700

    Fixed Compare-Object logic
    Compare-object logic was missing proper
    InputObject evalutaion. Once added users already
    added to the Employee-Password-Policy group
    were not re-processed.

--- Update-EmployeePasswordPolicyGroup.ps1
+++ Update-EmployeePasswordPolicyGroup.ps1
@@ -6,40 +6,43 @@
 .OUTPUTS
 .NOTES
 #>
- [cmdletbinding()]
- param ( 
-  [Parameter(Position=0,Mandatory=$True)]
-  [Alias('DC','Server')]
-  [string]$DomainController,
-  [Parameter(Position=1,Mandatory=$True)]
-  [Alias('ADCred')]
-  [System.Management.Automation.PSCredential]$Credential,
-  [Parameter(Position=3,Mandatory=$false)]
-  [SWITCH]$WhatIf
- )
+[cmdletbinding()]
+param ( 
+ [Parameter(Position = 0, Mandatory = $True)]
+ [Alias('DC', 'Server')]
+ [string]$DomainController,
+ [Parameter(Position = 1, Mandatory = $True)]
+ [Alias('ADCred')]
+ [System.Management.Automation.PSCredential]$Credential,
+ [Parameter(Position = 3, Mandatory = $false)]
+ [SWITCH]$WhatIf
+)
 
-$adCmdLets = 'Get-ADUser','Get-ADGroupMember','Add-ADGroupMember'
+$adCmdLets = 'Get-ADUser', 'Get-ADGroupMember', 'Add-ADGroupMember'
 $adSession = New-PSSession -ComputerName $DomainController -Credential $Credential
 Import-PSSession -Session $adSession -Module ActiveDirectory -CommandName $adCmdLets -AllowClobber
 
-$aDParams = @{ 
- Filter = {
-   (mail -like "*@*") -and
-   (employeeID -like "*")
+$aDParams = @{
+ Filter     = {
+  (mail -like "*@*") -and
+  (employeeID -like "*")
  }
- Searchbase = 'OU=Employees,OU=Users,OU=Domain_Root,DC=chico,DC=usd'
  Properties = 'employeeId'
+ Searchbase = 'OU=Employees,OU=Users,OU=Domain_Root,DC=chico,DC=usd'
 }
-$staffSams = (Get-Aduser @aDParams | Where-Object{ $_.employeeId -match "\d{4,}" }).samAccountName
+$staffSams = (Get-Aduser @aDParams | Where-Object { $_.employeeId -match "\d{4,}" }).samAccountName
 $groupSams = (Get-ADGroupMember -Identity 'Employee-Password-Policy').SamAccountName
 
-$missingSams = (Compare-Object -ReferenceObject $groupSams -DifferenceObject $staffSams).InputObject
+$missingSams = Compare-Object -ReferenceObject $groupSams -DifferenceObject $staffSams | 
+Where-Object { $_.SideIndicator -eq '=>' }
 if ($missingSams) {
- "Missing user objects`:"
- $missingSams
  "Adding missing user objects to Employee-Password-Policy group."
- Add-ADGroupMember -Identity 'Employee-Password-Policy' -Members $missingSams -WhatIf:$WhatIf
-} else { "Employee-Password-Policy security group has no missing user objects." }
+ foreach ($user in ($missingSams).InputObject) {
+  $user
+ }
+ Add-ADGroupMember -Identity 'Employee-Password-Policy' -Members ($missingSams).InputObject -WhatIf:$WhatIf
+}
+else { "Employee-Password-Policy security group has no missing user objects." }
 
 'Tearing down sessions...'
 Get-PSSession | Remove-PSSession
